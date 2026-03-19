dd2e6d1d20a9d4e24a01587df6aca4fd0b7ed550
Manoj Thangamani <manoj.thangamani@psiog.com>
2026-03-19 22:07:09 +0530

    updated database to rds

--- src/config/env.js
+++ src/config/env.js
@@ -5,5 +5,5 @@ module.exports = {
   NODE_ENV: process.env.NODE_ENV || "development",
   JWT_SECRET: process.env.JWT_SECRET || "fallback_secret",
   REDIS_URL: process.env.REDIS_URL || "redis://localhost:6379",
-  USE_REDIS: process.env.USE_REDIS === "false",
+  USE_REDIS: process.env.USE_REDIS === "true",
 };
